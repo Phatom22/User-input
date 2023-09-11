049cbcf641d00f2e2685f17af9a583bb318850e0
nomvula <nzshabangu95@gmail.com>
2023-09-11 14:31:00 +0200

    Add project files.

---
+++ User input/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your name ");
+            string firstname = Console.ReadLine();
+
+            Console.WriteLine("hi " + firstname);
+
+            Console.WriteLine("enter your age");
+            string age = Console.ReadLine();
+            Console.WriteLine("you have " + age + "years ");
+            //freeze console
+            Console.ReadLine();
+        }
+    }
+}
